7362d136b56d48fd31796e17cc6299c2b43c3ebb
Rakesh <rakesh.singh2@go-mmt.com>
2023-10-28 20:13:45 +0530

    pass request query parameters to target host

---
+++ src/pages/api/test/index.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const queries = req.query;
+    const body = req.body;
+
+    /** Get test */
+    if (req.method === 'GET') {
+
+        if (queries.op === "1") {
+            return res.status(200).send('1');
+        }
+
+        if (queries.op === "2") {
+            return res.status(200).send('2');
+        }
+
+        return res.status(200).send('get');
+    }
+
+    /** POST test */
+    if (req.method === 'POST') {
+        return res.status(200).send(body);
+    }
+}
+
+
+export default handler;
